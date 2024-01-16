26c49d272393caa10e336d3443861d9f8574bbaf
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-01-16 15:07:25 +0000

    CRUDed school

---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+insert into students (
+    first_name, 
+    last_name, 
+    phone
+    ) VALUES (
+        'Ben',
+        'Block',
+        '111-222-333'
+    )
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students
+SET phone = '202-392-4892'
+WHERE id = 1;
